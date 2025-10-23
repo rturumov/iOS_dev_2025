12228b9f7a3ba0b788e1678b05ddc5c9e9542334
rturumov <rasul.turum.2004@gmail.com>
2025-10-23 06:26:17 +0500

    lab5

---
+++ lab5/RandomizerApp/RandomizerAppTests/RandomizerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomizerAppTests.swift
+//  RandomizerAppTests
+//
+//  Created by Rasul Turumov on 23.10.2025.
+//
+
+import Testing
+@testable import RandomizerApp
+
+struct RandomizerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
